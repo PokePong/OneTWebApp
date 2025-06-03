622aa336871245f3652130a859913c1ef53c3020
MatthieuLeCoz <matthlecoz@hotmail.fr>
2025-06-03 17:38:06 +0200

    Replacing tchap by visio and putting right favicon

--- OneTWebApp/OneTWebApp.Client/Utils/ImageUtils.cs
+++ OneTWebApp/OneTWebApp.Client/Utils/ImageUtils.cs
@@ -5,7 +5,7 @@ namespace OneTWebApp.Client.Utils;
 public static class ImageUtils {
     public static string GetIconAppPath(AppType type) => type switch {
         AppType.Docs => "Images/icon-docs.png",
-        AppType.Meet => "Images/icon-tchap.png",
+        AppType.Meet => "Images/icon-visio.png",
         _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
     };
 }
